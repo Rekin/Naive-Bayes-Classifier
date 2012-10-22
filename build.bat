7a919c8657afce916edfaf050882e88fef8e59cb
Rekin <mr.rekin@gmail.com>
2012-10-22 21:24:30 +0200

    Add build bat file

---
+++ build.bat
@@ -0,0 +1,5 @@
+cd C:\Repositories\Github\Naive-Bayes-Classifier\NaiveBayesClassifier\
+msbuild NaiveBayesClassifier.sln
+cd C:\Repositories\Github\Naive-Bayes-Classifier\UsageExampleWindows\
+msbuild UsageExampleWindows.sln
+PAUSE
